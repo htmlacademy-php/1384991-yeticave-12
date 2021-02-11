2a57780efb08fb3279db73f6a92f7a9bf72fb0d1
Oleh <informsite2017@gmail.com>
2021-02-11 18:13:28 +0200

    Внес правки

--- lot.php
+++ lot.php
@@ -22,7 +22,11 @@ foreach ($categories_from_db as $item) {
 }
 
 // запрос на получение информации о лоте
-$get_lot_sql = "SELECT * FROM lots WHERE id=?";
+$get_lot_sql = "SELECT lots.*, COALESCE (max(bets.bet_price), lots.start_price) AS current_price, categories.cat_name FROM lots 
+LEFT JOIN bets ON lots.id = bets.lot_id 
+JOIN categories ON lots.cat_id = categories.id
+WHERE lots.id=?
+GROUP BY lots.id";
 $stmt_lot = $db_connect->prepare($get_lot_sql);
 $stmt_lot->bind_param("s", $_GET['id']);
 $stmt_lot->execute();
@@ -32,7 +36,9 @@ $row_lot = $result_lot->fetch_assoc();
 // выводим 404 если нет параметра запроса или id не существует
 if(!isset($_GET['id']) || !$row_lot) {
 	http_response_code(404);
-	header("Location: ../pages/404.html");
+	$get_404 = include_template('404.php', ['page_title' => 'Страница не существует', 'categories_arr' => $categories_arr]);
+	$layout_content = include_template('layout.php', ['page_content' => $get_404, 'page_title' => 'Страница не существует', 'user_name' => $user_name, 'is_auth' => $is_auth, 'categories_arr' => $categories_arr]);
+	print $layout_content;
 	exit;
 }
 
@@ -47,21 +53,9 @@ $stmt_bets->execute();
 $result_bets = $stmt_bets->get_result();
 $list_bets = $result_bets->fetch_all(MYSQLI_ASSOC);
 
-// ищем максимальную ставку на лот
-$max_bet_id = count($list_bets) - 1;
-
-if ($max_bet_id >= 0) {
-	$max_bet = $list_bets[$max_bet_id]['bet_price'];
-}
-
-// определяем текущую цену лота
-if (isset($max_bet)) {
-	$bet_price = $max_bet;
-} else {
-	$bet_price = $row_lot['start_price'];
-}
-
 if (isset($_GET['id'])) {
-$lot_content = include_template('lot_template.php', ['row_lot' => $row_lot, 'list_bets' => $list_bets, 'categories_arr' => $categories_arr, 'bet_price' => $bet_price , 'user_name' => $user_name, 'page_title' => $row_lot['name_lot'], 'is_auth' => $is_auth]);
+$page_content = include_template('lot_template.php', ['row_lot' => $row_lot, 'list_bets' => $list_bets, 'categories_arr' => $categories_arr]);
 }
-print $lot_content;
+
+$layout_content = include_template('layout.php', ['page_content' => $page_content, 'page_title' => $row_lot['name_lot'], 'categories_arr' => $categories_arr, 'user_name' => $user_name, 'is_auth' => $is_auth]);
+print $layout_content;
