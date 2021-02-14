ace345238af300bea4fd84beb6231a443d0edba7
Oleh <informsite2017@gmail.com>
2021-02-14 16:53:37 +0200

    Внес правки в lot

--- lot.php
+++ lot.php
@@ -1,17 +1,22 @@
 <?php
 require 'connections.php';
-if (isset($_GET['id'])) {
+
+if (!isset($_GET['id'])) {
+	get_404($categories_arr, $user_name, $is_auth);
+}
 // запрос на получение информации о лоте
-	$get_lot_sql = "SELECT lots.*, COALESCE (max(bets.bet_price), lots.start_price) AS current_price, categories.cat_name FROM lots 
-	LEFT JOIN bets ON lots.id = bets.lot_id 
-	JOIN categories ON lots.cat_id = categories.id
-	WHERE lots.id=?
-	GROUP BY lots.id";
-	$stmt_lot = $db_connect->prepare($get_lot_sql);
-	$stmt_lot->bind_param("s", $_GET['id']);
-	$stmt_lot->execute();
-	$result_lot = $stmt_lot->get_result();
-	$row_lot = $result_lot->fetch_assoc();
+$get_lot_sql = "SELECT lots.*, COALESCE (max(bets.bet_price), lots.start_price) AS current_price, categories.cat_name FROM lots 
+LEFT JOIN bets ON lots.id = bets.lot_id 
+JOIN categories ON lots.cat_id = categories.id
+WHERE lots.id=?
+GROUP BY lots.id";
+$stmt_lot = $db_connect->prepare($get_lot_sql);
+$stmt_lot->bind_param("s", $_GET['id']);
+$stmt_lot->execute();
+$result_lot = $stmt_lot->get_result();
+$row_lot = $result_lot->fetch_assoc();
+if (!$row_lot) {
+	get_404($categories_arr, $user_name, $is_auth);
 }
 
 // запрос на получение списка ставок
@@ -24,11 +29,6 @@ $stmt_bets->bind_param("s", $_GET['id']);
 $stmt_bets->execute();
 $result_bets = $stmt_bets->get_result();
 $list_bets = $result_bets->fetch_all(MYSQLI_ASSOC);
-
-// выводим 404 если нет параметра запроса или id не существует
-if (!isset($_GET['id']) || !isset($row_lot)) {
-	get_404($categories_arr, $user_name, $is_auth);
-}
 
 $page_content = include_template('lot_template.php', ['row_lot' => $row_lot, 'list_bets' => $list_bets, 'categories_arr' => $categories_arr]);
 
