a6181a730bc39ea2ed0ca56b88594a924b0a0400
Oleh <informsite2017@gmail.com>
2021-02-25 17:21:21 +0200

    Внес правки

--- login.php
+++ login.php
@@ -4,34 +4,45 @@ if (isset($_SESSION['user'])) {
   	get_403($categories_arr);
 }
 //Валидация формы входа
+
+
+/*
+1. Валидируем входящие данные
+1.1 Проверяем на ошибки поля
+1.1.1 Если пустой маил - заполните поле
+1.1.2 Если не пустой маил - валидируем поле
+1.1.3 Если нет @ - некорректный маил
+1.1.4 Если пустой пароль - заполните поле
+
+2 Если ошибок нет - выполняем запрос в базу
+2.1 Если имэйл не найден - такого пользователя не существует
+2.1.1 Если найден - сравниваем пароль с паролем в базе
+2.1.2 Если не совпал - неверный пароль
+2.1.3 Если совпал - авторизируем
+*/
 $err = [];
 if (!empty($_POST)) {
-	if (!empty($_POST['email']) AND !empty($_POST['password'])) {
-		if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$err['email'] = 'Введен некорректный email';
+	if (empty($_POST['email'])) {
+		$err['email'] = 'Введите Email';
+	} elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$err['email'] = 'Некорректный Email';
+	} 
+	if (empty($_POST['password'])) {
+		$err['password'] = 'Введите пароль';
+	} 
+	if (empty($err)) {
+		$checkEmail = "SELECT * FROM users WHERE user_email = ?";
+		$stmt = getSqlPrepare($db_connect, $checkEmail, [$_POST['email']]);
+		$result = $stmt->get_result();
+		$checkArr = $result->fetch_assoc();
+		if (!$checkArr) {
+			$err['email'] = 'Пользователь с таким Email не найден';
+		} elseif (!password_verify($_POST['password'], $checkArr['password'])) {
+			$err['password'] = 'Неверный пароль';
 		} else {
-			$checkEmail = "SELECT * FROM users WHERE user_email = ?";
-			$stmt = getSqlPrepare($db_connect, $checkEmail, [$_POST['email']]);
-			$result = $stmt->get_result();
-			$checkArr = $result->fetch_assoc();
-			if (!isset($checkArr['user_email'])) {
-				$err['email'] = 'Email не найден';
-			} else {
-				if (!password_verify($_POST['password'], $checkArr['password'])) {
-					$err['password'] = 'Пароль введен неверно!';
-				} else {
-					$_SESSION['user'] = $checkArr;
-					header("Location: /");
-					exit;
-				}
-			}
-		} 
-	} else {
-		if (empty($_POST['email'])) {
-			$err['email'] = 'Введите Email';
-		} 
-		if (empty($_POST['password'])) {
-			$err['password'] = 'Введите пароль';
+			$_SESSION['user'] = $checkArr;
+			header("Location: /");
+			exit;
 		}
 	}
 }
--- sign_up.php
+++ sign_up.php
@@ -5,56 +5,53 @@ if (isset($_SESSION['user'])) {
 }
 $err = [];
 
+/*
+1. Проверяем поля на ошибки
+1.1. Если пустой майл - введите майл
+1.2. Если не пустой майл - валидируем поле
+1.3. Если нет @ - некорректный майл
+1.4. Если пустой пароль - укажите пароль
+1.5. Если пустой нейм - введите имя
+1.6. Если пустой месседж - введите контакты
+
+Если нет ошибок - выполняем запрос
+
+2. Выполняем запрос в базу на проверку маила
+2.1 если майл существует - пользователь с таким майлом уже зарегистрирован
+2.2. если майл не существует - выполняем запрос на добавление нового пользователя
+*/
+
 if (!empty($_POST)) {
-	//Валидация формы регистрации
-	function checkFieldReg($param) {
-		$err = [];
-		foreach ($param as $key => $value) {
-			if ($key == 'email') {
-				if(empty($value)) {
-					$err[$key] = 'Введите email';
-				} elseif(!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-					$err[$key] = 'Введен некорректный email';
-				}
-			}
-			elseif ($key == 'password') {
-				if(empty($value)) {
-					$err[$key] = 'Укажите пароль';
-				}
-			}
-
-			elseif ($key == 'name') {
-				if(empty($value)) {
-					$err[$key] = 'Введите имя';
-				}
-			}
-			elseif ($key == 'message') {
-				if(empty($value)) {
-					$err[$key] = 'Укажите свои контактные данные';
-				}
-			}
+	if (empty($_POST['email'])) {
+		$err['email'] = 'Введите Email';
+	} elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$err['email'] = 'Введен некорректный email';
+	}
+	if (empty($_POST['password'])) {
+		$err['password'] = 'Укажите пароль';
+	}
+	if (empty($_POST['name'])) {
+		$err['name'] = 'Введите имя';
+	}
+	if (empty($_POST['message'])) {
+		$err['message'] = 'Укажите свои контактные данные';
+	}
+	if (empty($err)) {
+		$checkEmail = "SELECT user_email FROM users WHERE user_email = ?";
+		$stmt = getSqlPrepare($db_connect, $checkEmail, [$_POST['email']]);
+		$result = $stmt->get_result();
+		$email = $result->fetch_assoc();
+		if ($email) {
+			$err['email'] = 'Пользователь с таким email уже зарегистрирован!';
+		} else {
+			$sql = "INSERT INTO users SET user_email = ?, user_name = ?, password = ?, contacts = ?";
+			getSqlPrepare($db_connect, $sql, [$_POST['email'], $_POST['name'], password_hash($_POST['password'], PASSWORD_DEFAULT), $_POST['message']]);
+			header("Location: /login.php");
+	  		exit;
 		}
-		return $err;
 	}
-	$err = checkFieldReg(['email' => $_POST['email'], 'password' => $_POST['password'], 'name' => $_POST['name'], 
-		'message' => $_POST['message']]);
 
-	$checkEmail = "SELECT user_email FROM users WHERE user_email = ?";
-	$stmt = getSqlPrepare($db_connect, $checkEmail, [$_POST['email']]);
-	$result = $stmt->get_result();
-	$email = $result->fetch_assoc();
-	if (isset($email)) {
-		$err['email'] = 'Пользователь с таким email уже зарегистрирован!';
-	}
-	if ($err == false) {
-		$sql = "INSERT INTO users SET user_email = ?, user_name = ?, password = ?, contacts = ?";
-		getSqlPrepare($db_connect, $sql, [$_POST['email'], $_POST['name'], password_hash($_POST['password'], PASSWORD_DEFAULT), $_POST['message']]);
-		header("Location: /login.php");
-  		exit;
-	}
 }
-
-
 $page_content = include_template('sign-up_template.php', ['err' => $err]);
 
 $layout_content = include_template('layout.php', ['page_content' => $page_content, 'page_title' => 'Регистрация нового пользователя', 
